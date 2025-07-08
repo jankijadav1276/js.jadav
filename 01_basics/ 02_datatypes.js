4a6bd8e03b55b90381e0ceb086889798f3c9243e
jankijadav1276 <jankijadav1276@gmail.com>
2025-07-08 09:36:40 +0000

    basics of js from day 2&3

---
+++ 01_basics/ 02_datatypes.js
@@ -0,0 +1,21 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3 +3) // we are using node js not browser
+// console.log(3 + 3)
+
+// console.log("Janki")
+//MDM website for content
+
+let name = "janki" //string datatype 
+let age = 18 //number datatype
+let isLoogedIn = false //boolen datatype
+
+//number => 2 to power 53(range)
+//bigint //string => ""
+//boolean => true/false 
+//null=> standalone value
+// undefined // symbol => unique
+
+//object
+//null is object 
+console.log(typeof age);
---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,54 @@
+let score = "janki"
+
+//console.log(typeof score);
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+//"33"=> 33
+//"33abc"=> NaN
+//true =>1; false=>0
+let isLoogedIn = "janki"
+
+let booleanIsLoggedIn = Boolean(isLoogedIn)
+//console.log(booleanIsLoggedIn);
+
+// 1=> true; 0=>false
+//""=>false ; "janki"=>true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+//****************************************************** Operations ******************************************************************************
+
+let value = 3
+let negvalue = -value
+//console.log(negvalue);
+
+// console.log(2+2);
+// console.log(2-2);
+
+let str1 = "hello"
+let str2 = " janki"
+
+let str3 = str1+str2
+//console.log(str3);
+
+//ACMA Script website
+// console.log("1" +2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 +2+"2");
+
+// console.log((3+4)*5%3);
+
+// console.log(+true);
+// console.log(+"");
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
